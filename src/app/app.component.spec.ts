846764e6f0cd970beae985a37744c1ae6d481c20
unknown <fzikovski36@yahoo.com>
2024-01-03 17:52:50 +0100

    Added Todo, Notes and Route Transitions

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,6 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-personal-dashboard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-personal-dashboard');
   });
 
   it('should render title', () => {
